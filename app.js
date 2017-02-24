8c022b2e615fde844445cb428bfa41c010775367
moeidsaleem <moeidsaleem@gmail.com>
2017-02-24 15:26:38 +0500

    electron first

---
+++ app.js
@@ -0,0 +1,22 @@
+
+const electron = require('electron');
+const app = electron.app;
+
+const path = require('path');
+
+const url = require('url');
+
+const BrowserWindow = electron.BrowserWindow;
+
+var mainWindow;
+app.on('ready', function(){
+	mainWindow = new BrowserWindow({width:1024, height: 760, backgroundColor:'#fece1a'});
+	// mainWindow.loadURL('https://github.com');
+  mainWindow.loadURL(url.format({
+  	pathname: path.join(__dirname, 'index.html'),
+  	protocol:'file:',
+  	slashes: true
+  }));
+
+
+});
